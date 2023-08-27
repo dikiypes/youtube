5c8262ba179c7ad940e6764f8ad912a9f0362d93
Evgeniy <Darkorbitint1@yandex.ru>
2023-08-27 22:45:33 +0300

    3333

--- src/channel.py
+++ src/channel.py
@@ -11,7 +11,8 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
 
         # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-        api_key: str = os.getenv('ApiKey')
+        # api_key: str = os.getenv('ApiKey')
+        api_key: str = 'AIzaSyAlkREa53g8ujIlII3doGa_lxEKzS3EMEM'
 
         # создать специальный объект для работы с API
         self.youtube = build('youtube', 'v3', developerKey=api_key)
@@ -45,3 +46,42 @@ class Channel:
 
         with open(filename, "w", encoding="utf-8") as file:
             json.dump(bookjson, file, ensure_ascii=False)
+
+
+    def __str__(self):
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        if isinstance(other, Channel):
+            return int(self.subscriberCount) + int(other.subscriberCount)
+        raise TypeError("Unsupported operand type for +: 'Channel' and {}".format(type(other)))
+
+    def __sub__(self, other):
+        if isinstance(other, Channel):
+            return int(self.subscriberCount) - int(other.subscriberCount)
+        raise TypeError("Unsupported operand type for -: 'Channel' and {}".format(type(other)))
+
+    def __lt__(self, other):
+        if isinstance(other, Channel):
+            return int(self.subscriberCount) < int(other.subscriberCount)
+        raise TypeError("Unsupported operand type for <: 'Channel' and {}".format(type(other)))
+
+    def __le__(self, other):
+        if isinstance(other, Channel):
+            return int(self.subscriberCount) <= int(other.subscriberCount)
+        raise TypeError("Unsupported operand type for <=: 'Channel' and {}".format(type(other)))
+
+    def __eq__(self, other):
+        if isinstance(other, Channel):
+            return int(self.subscriberCount) == int(other.subscriberCount)
+        return False
+
+    def __gt__(self, other):
+        if isinstance(other, Channel):
+            return int(self.subscriberCount) > int(other.subscriberCount)
+        raise TypeError("Unsupported operand type for >: 'Channel' and {}".format(type(other)))
+
+    def __ge__(self, other):
+        if isinstance(other, Channel):
+            return int(self.subscriberCount) >= int(other.subscriberCount)
+        raise TypeError("Unsupported operand type for >=: 'Channel' and {}".format(type(other)))
