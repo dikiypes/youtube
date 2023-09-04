6955926991adb112693305c3ef87ddea7a0e5174
Evgeniy <Darkorbitint1@yandex.ru>
2023-09-04 15:54:01 +0300

    6666

--- src/video.py
+++ src/video.py
@@ -8,16 +8,21 @@ class Video:
         api_key: str = 'AIzaSyAlkREa53g8ujIlII3doGa_lxEKzS3EMEM'
         youtube = build('youtube', 'v3', developerKey=api_key)
 
-        video = youtube.videos().list(part='snippet,statistics', id=video_id).execute()
-        video_snippet = video["items"][0]["snippet"]
-        video_statistics = video["items"][0]["statistics"]
-
         self.id = video_id
-        self.title = video_snippet["title"]
-        self.url = f"https://www.youtube.com/watch?v={video_id}"
-        self.viewCount = video_statistics["viewCount"]
-        self.likeCount = video_statistics["likeCount"]
+        try:
+            video = youtube.videos().list(part='snippet,statistics', id=video_id).execute()
+            video_snippet = video["items"][0]["snippet"]
+            video_statistics = video["items"][0]["statistics"]
+            self.title = video_snippet["title"]
+            self.url = f"https://www.youtube.com/watch?v={video_id}"
+            self.viewCount = video_statistics["viewCount"]
+            self.like_count = video_statistics["likeCount"]
 
+        except:
+            self.title = None
+            self.url = None
+            self.viewCount = None
+            self.like_count = None
     def __str__(self):
         return self.title
 
